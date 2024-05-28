ed13e7ca4cf620c0833be8f31e2af4df7d69cf6b
rohitkumar-yadav <rohitkumar.yadav@goodworklabs.com>
2024-05-28 16:15:35 +0530

    db fix

--- db/config.js
+++ db/config.js
@@ -3,11 +3,11 @@ const dotenv =  require('dotenv');
 dotenv.config();
 
 const pgconnect = new Client({
-    user: process.env.USER,
-    host: process.env.HOST,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD,
-    port: parseInt(process.env.PG_PORT),
+    user: process.env.USERr || 'admin',
+    host: process.env.HOSTt || 'luckily-valid-blowfish-iad.a1.pgedge.io',
+    database: process.env.DATABASES || 'adtech_db',
+    password: process.env.PASSWORDD || '9GUhRUHV560Fa27q12xbu2l8',
+    port: parseInt(process.env.PG_PORTS) || 5432,
     ssl: {
       rejectUnauthorized: false
     }
