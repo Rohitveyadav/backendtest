4a6f27c698fffbe4bee1bfafac9936bf704f49b1
rohitkumar-yadav <rohitkumar.yadav@goodworklabs.com>
2024-05-28 15:01:49 +0530

    restructure project

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const dotenv = require("dotenv");
 
-dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 9000;
+
 
 app.get("/", (req,res)=> {
     res.send("Hello there! Api is working")
 })
 
-app.listen(PORT, () => console.log(`Sever is running port ${PORT} ...`));
+// app.listen(PORT, () => console.log(`Sever is running port ${PORT} ...`));
+
+module.exports =  app;
